268250778473b2d0767001a63754feadb835283c
asierra <algsierra@gmail.com>
2024-12-30 16:06:35 -0500

    message

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,8 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deletebook(${index})")">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -37,4 +38,7 @@ function showbooks() {
             document.getElementById('pagesNumber').value = '';
  }
 
- 
+function deletebook(index) {
+    books.splice(index, 1);
+    showbooks();
+}
